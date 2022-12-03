1bf5d28f9282b3ba3020ca5fb7e98a90ee347b39
Ravi-Horatti <31774423+Ravi-Horatti@users.noreply.github.com>
2022-12-03 08:49:16 +0530

    Create Exponential.cs

---
+++ Exponential.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter the base:");
+          double num1 = Convert.ToDouble(Console.ReadLine());
+       
+          Console.WriteLine("Enter the power :");
+          double num2 = Convert.ToDouble(Console.ReadLine());
+          
+
+         Console.WriteLine(getPow(num1,num2));
+         }
+         static double getPow(double basen, double pow)
+         {
+         	double result=1;
+         	for(int i=0;i<= pow-1 ;i++)
+         	{
+         		result= result*basen;
+         	}
+
+         	return result;
+         }
+
+    }
+}

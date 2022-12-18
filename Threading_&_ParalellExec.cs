e116880d71083851a918d4242abec507957100bf
Ravi-Horatti <31774423+Ravi-Horatti@users.noreply.github.com>
2022-12-18 19:52:36 +0530

    Update Threading_&_ParalellExec.cs

--- Threading_&_ParalellExec.cs
+++ Threading_&_ParalellExec.cs
@@ -9,33 +9,37 @@ namespace giraffe
     {
         static void Main(string[] args)
         {
-          Thread T1= new Thread(new ThreadStart(fun1));
-          Thread T2= new Thread(new ThreadStart(fun2));
+           // Thread T1 = new Thread(new ThreadStart(fun1));
+            Task T1 = new Task(fun1);
+            Task T1 = new Task(fun1); //Built on top of Thread // Dosnt contain Thread Affinity. like Thread.
+           // Thread T2 = new Thread(new ThreadStart(fun2));
 
             T1.Start();
             T2.Start();
             Console.WriteLine("Helo World.");
             Console.ReadLine();
-         
+
         }
-         static void fun1()
-         {
-            for(int i=0;i<10;i++){
+        static void fun1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
                 Thread.Sleep(1000);
-                 Console.WriteLine("Fuun 1 --:"+ i);
+                Console.WriteLine("Fuun 1 --:" + i);
             }
 
-         }
+        }
 
-          static void fun2()
-         {
-            for(int i=0;i<20;i++){
+        static void fun2()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
                 Thread.Sleep(1000);
-                   Console.WriteLine("Fuun 2 --:"+ i);
+                Console.WriteLine("Fuun 2 --:" + i);
             }
- 
-         }
-        
+
+        }
+
     }
 
 
